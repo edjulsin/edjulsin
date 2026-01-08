60b54920cceb861ee31007b4c3b2c3be7f335aa7
edjulsin <edjulsin@gmail.com>
2026-01-08 22:20:24 +0700

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const development = process.env.NODE_ENV === 'development'
 
 const CSP = `
     default-src 'self';
-    script-src 'self' ${development ? "'unsafe-eval' 'unsafe-inline'" : ''};
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
