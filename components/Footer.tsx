0c83a93c4584ab7118a4e6609fae37e07fcf328f
edjulsin <edjulsin@gmail.com>
2026-03-24 03:13:36 +0700

    fixed date error on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,8 @@
 'use client'
 
+
 const Footer = () => {
-	const today = new Date()
+	const today = new Date().getFullYear() + ''
 	return (
 		<footer className='text-neutral-500 font-medium text-center tabular-nums min-h-[30dvh] flex flex-col gap-y-5 md:gap-y-10 xl:gap-y-20 justify-center items-center'>
 			<span className='text-base md:text-xl xl:text-3xl'>
@@ -9,8 +10,8 @@ const Footer = () => {
 				{' '}
 				<span className='tracking-tight'>(UTC + 7)</span>
 			</span>
-			<time dateTime={today.toISOString()} className='text-sm md:text-lg xl:text-2xl'>
-				{today.getFullYear()}
+			<time dateTime={today} className='text-sm md:text-lg xl:text-2xl'>
+				{today}
 			</time>
 		</footer>
 	)
