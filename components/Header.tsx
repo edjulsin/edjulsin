a9ef74a234902a7e0c9099dafed41894b4a3a9de
edjulsin <edjulsin@gmail.com>
2026-04-25 02:08:44 +0700

    resume download added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 
 const Header = () => (
-	<header className='tracking-tight text-2xl md:text-3xl xl:text-4xl text-neutral-400 text-center xl:text-left min-h-[20dvh] font-extralight'>
-		<nav>
-			<Link href='/'>Edwin Julian</Link>
+	<header className='tracking-tight text-center xl:text-left min-h-[20dvh] font-extralight'>
+		<nav className='flex items-center justify-between gap-3'>
+			<Link href='/' className='text-neutral-400 text-2xl md:text-3xl xl:text-4xl'>Edwin Julian</Link>
+			<a href='/resume.pdf' download='Edwin_Julian_Frontend_Resume' className='flex items-center gap-1.5 px-3 py-1.5 text-sm md:text-base xl:text-lg rounded-full bg-foreground text-background font-semibold transition-colors'>
+				<span>Resume</span>
+				<span className='size-5 flex items-center justify-center rounded-full outline-1 outline-background'>&#8595;</span>
+			</a>
 		</nav>
 	</header>
 )
