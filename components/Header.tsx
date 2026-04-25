bf5b504f73efcc0b6ca3d2be746d7800a11e1e59
edjulsin <edjulsin@gmail.com>
2026-04-25 07:13:12 +0700

    resume header updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ const Header = () => (
 	<header className='tracking-tight text-center xl:text-left min-h-[20dvh] font-extralight'>
 		<nav className='flex items-center justify-between gap-3'>
 			<Link href='/' className='text-neutral-400 text-2xl md:text-3xl xl:text-4xl'>Edwin Julian</Link>
-			<a href='/resume.pdf' download='Edwin_Julian_Frontend_Resume' className='flex items-center gap-1.5 px-3 py-1.5 text-sm md:text-base xl:text-lg rounded-full bg-foreground text-background font-semibold transition-colors'>
-				<span>Resume</span>
-				<span className='size-5 flex items-center justify-center rounded-full outline-1 outline-background'>&#8595;</span>
+			<a href='/resume.pdf' download='Edwin_Julian_Frontend_Resume' className='flex items-center gap-1.5 px-3 py-1.5 text-sm md:text-base xl:text-lg rounded-full bg-foreground font-semibold transition-colors'>
+				<span className='text-background'>Resume</span>
+				<span className='size-5 flex items-center justify-center rounded-full outline-1 outline-neutral-500 text-neutral-500'>&#8595;</span>
 			</a>
 		</nav>
 	</header>
